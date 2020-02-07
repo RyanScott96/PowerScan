288e79512c5330552c7e1c991994973c2e0ffe9f
Ryan L Scott <r.l.scott2014@gmail.com>
2020-02-07 18:07:51 -0500

    Completed basic scan, email, delete functionality

--- PowerScan.ps1
+++ PowerScan.ps1
@@ -1,1 +1,26 @@
-﻿Write-Host "Congratulations! Your first script executed successfully"
+﻿#Config Variables
+$PSEmailServer = "localhost"
+$Domain = "localhost"
+$FromId = "power_scan@$Domain"
+$ToId = "rlscott@$Domain"
+
+#Set date time to current time
+$Date = Get-Date -Format "dd-mm-yyyy"
+$Time = Get-Date -Format "HH:mm:ss"
+$DateTime = "$Date|$Time"
+
+#Find all .pdfs that follow the format build#.pdf in the $PATH
+$ScannedItems = Get-ChildItem -Path ./tests | Where-Object -Property Name -Match "build\d+.pdf"
+
+#For each .pdf create an email with the # as the ISSUE NUMBER
+#and .pdf file as the attachment
+$ScannedItems | ForEach-Object -Process {
+
+    #Ticket number should be in the file name 
+    $Number = $_.Name -replace '\D+(\d+).pdf', '$1'
+    $Subject = "Attach ISSUE=545-$Number"
+    $Body = "Scanned build sheets batch uploaded at $DateTime"
+    $_ | Send-MailMessage -From $FromId -To $ToId -Subject $Subject -Body $Body
+}
+
+$ScannedItems | Remove-Item
