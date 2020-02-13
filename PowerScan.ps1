716d9dc27408b91536ef22b3972dd8965959cb3c
Ryan L Scott <r.l.scott2014@gmail.com>
2020-02-13 13:34:52 -0500

    Replaced Name with FullName

--- PowerScan.ps1
+++ PowerScan.ps1
@@ -1,6 +1,6 @@
 ﻿#Config Variables
 $PSEmailServer = "localhost"
-$Path = "./tests"
+$Path = "./test"
 #Insecure so keep messaging to an internal network
 $Domain = "localhost"
 $FromId = "power_scan"
@@ -12,14 +12,14 @@ $Time = Get-Date -Format "HH:mm:ss"
 $DateTime = "$Date|$Time"
 
 #Find all .pdfs that follow the format build#.pdf in the $PATH
-$ScannedItems = Get-ChildItem -Path $Path | Where-Object -Property Name -Match ".*\d+.*.pdf"
+$ScannedItems = Get-ChildItem -Path $Path | Where-Object -Property FullName -Match ".*\d+.*.pdf"
 
 #For each .pdf create an email with the # as the ISSUE NUMBER
 #and .pdf file as the attachment
 $ScannedItems | ForEach-Object -Process {
 
     #Ticket number should be in the file name 
-    $Number = $_.Name -replace '\D+(\d+).pdf', '$1'
+    $Number = $_.FullName -replace '\D+(\d+).pdf', '$1'
     $Subject = "Attach ISSUE=545-$Number"
     $Body = "Scanned build sheets batch uploaded at $DateTime"
     $_ | Send-MailMessage -From "$FromId@$Domain" -To "$ToId@$Domain" -Subject $Subject -Body $Body
